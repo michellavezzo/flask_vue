f58329c805577f5628c4f6352829c76a31dbc662
Michel Lavezzo <michellavezzo@gmail.com>
2025-03-10 02:38:55 -0300

    lint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/typescript/recommended",
+        "plugin:prettier/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                tabWidth: 4,
+                useTabs: false,
+                endOfLine: "auto",
+            },
+        ],
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "vue/multi-word-component-names": "off",
+    },
 };
