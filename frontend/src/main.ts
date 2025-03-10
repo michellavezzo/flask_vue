39c6bbc4e8a38d8c06717b6dcae2c86b5edb4878
Michel Lavezzo <michellavezzo@gmail.com>
2025-03-10 02:47:13 -0300

    adding Vuetify + MDI icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,23 @@
 import { createApp } from "vue";
+
+// Vuetify
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+// Components
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "mdi",
+    },
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
