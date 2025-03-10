b0086c9e4775a3f2711544a5bdca1c80a3f8ecd3
Michel Lavezzo <michellavezzo@gmail.com>
2025-03-10 03:43:53 -0300

    global state for timezone (Choose what timezone the user will see)

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -5,6 +5,22 @@ const store = createStore({
     modules: {
         user: userModule,
     },
+    state: {
+        timezoneSelected: "",
+    },
+    mutations: {
+        SET_TIMEZONE(state, timezone) {
+            state.timezoneSelected = timezone;
+        },
+    },
+    actions: {
+        setTimezone({ commit }, timezone) {
+            commit("SET_TIMEZONE", timezone);
+        },
+    },
+    getters: {
+        timezoneSelected: (state) => state.timezoneSelected,
+    },
 });
 
 export default store;
